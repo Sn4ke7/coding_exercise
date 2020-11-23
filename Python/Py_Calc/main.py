850be433de798754920e131beb2e5835ae9e6de8
Filippo Dante Serpentis <dantes7m@gmail.com>
2020-11-23 23:36:04 +0100

    Just starting hello commit

---
+++ Python/Py_Calc/main.py
@@ -0,0 +1,10 @@
+def print_obiwan(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hello There... {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_obiwan('Filip')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
