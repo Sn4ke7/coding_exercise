1d9b00908b6be2ae90312a1ab6bd8cef33ac8ffc
Filippo Dante Serpentis <dantes7m@gmail.com>
2020-11-27 23:49:38 +0100

    Diving into basics of GUI

--- Python/Py_Calc/main.py
+++ Python/Py_Calc/main.py
@@ -4,21 +4,22 @@ from tkinter import ttk
 class Calculator:
     print("Test")
 
-    def __init__(self, root):
-        self.entry_value = StringVar(root, value="")
-        root = Tk()
-        root.geometry("1000x1000")
-        root.title("Calculator")
-        calc = Calculator(root)
-        root.mainloop()
+    # def __init__(self, root):
+    #     self.entry_value = StringVar(root, value="")
+    #     root = Tk()
+    #     root.geometry("1000x1000")
+    #     root.title("Calculator")
+    #     calc = Calculator(root)
+    #     root.mainloop()
 # TODO : Create and functionality for buttons
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    top = Tk()
-    top.mainloop()
-    top.geometry("1000x1000")
-    top.title("Calc")
+    calcGUI = Tk()
+    calcGUI.geometry("500x500")
+    calcGUI.title("Calc")
+    # mainloop at the end
+    calcGUI.mainloop()
     pass
 
 
