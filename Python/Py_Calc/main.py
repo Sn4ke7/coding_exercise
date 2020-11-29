47a528d51f89ea2a569829dc6f3b8f45fc82d8a3
Filippo Dante Serpentis <dantes7m@gmail.com>
2020-11-29 23:42:36 +0100

    Magic behind logic part 2

--- Python/Py_Calc/main.py
+++ Python/Py_Calc/main.py
@@ -14,6 +14,33 @@ class Calculator:
         self.number_entry.delete(0,"end")
         self.number_entrt.insert(0, entry_val)
 
+    def is_float(self,str_val):
+        try:
+            float(str_val)
+            return True
+        except ValueError:
+            return False
+    def math_button_press(self,value):
+        if self.is_float(str(self.number_entry.get())):
+            self.add_trigger = False
+            self.sub_trigger = False
+            self.mult_trigger = False
+            self.div_trigger = False
+            self.calc_value = float(self.entry_value.get())
+            if value == "/":
+                print("/ Pressed")
+                self.div_trigger = True
+            elif value == "*":
+                print("* Pressed")
+                self.div_trigger = True
+            elif value == "+":
+                print("+ Pressed")
+                self.add_trigger = True
+            else:
+                print("- Pressed")
+                self.sub_trigger = True
+            self.number_entry.delete(0,"end")
+
 if __name__ == '__main__':
     calcGUI = Tk()
     calcGUI.geometry("500x500")
